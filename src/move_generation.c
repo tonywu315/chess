b0c39ed2485e7661343cdc18c9f0e155e7be589c
Tony Wu <tonywu315@gmail.com>
2021-10-25 22:02:42 -0400

    Function refactor

--- src/move_generation.c
+++ src/move_generation.c
@@ -31,23 +31,6 @@ Move create_move(Fast start, Fast end, Fast flag) {
     return move;
 }
 
-/* Adds the move to the moves list */
-static void add_move(Move moves[], int count, Fast start, Fast end, Fast flag) {
-    moves[count] = create_move(start, end, flag);
-
-    /* For debugging */
-    if (false) {
-        if (flag == CASTLE_WK || flag == CASTLE_BK) {
-            printf("0-0\n");
-        } else if (flag == CASTLE_WQ || flag == CASTLE_BQ) {
-            printf("0-0-0\n");
-        } else {
-            printf("%c%d %c%d\n", (start & 7) + 'a', (start >> 4) + 1,
-                   (end & 7) + 'a', (end >> 4) + 1);
-        }
-    }
-}
-
 /* Checks if a square is attacked by the opponent of the player */
 int is_attacked(Fast square, Fast player) {
     /* Iterates through all the pieces */
@@ -97,25 +80,25 @@ int generate_moves(Move *moves) {
         if (board.castle & CASTLE_WK && board.pieces[F1] == EMPTY_PIECE &&
             board.pieces[G1] == EMPTY_PIECE && !is_attacked(E1, board.player) &&
             !is_attacked(F1, board.player) && !is_attacked(G1, board.player)) {
-            add_move(moves, count++, E1, G1, CASTLE_WK);
+            moves[count++] = create_move(E1, G1, CASTLE_WK);
         }
         if (board.castle & CASTLE_WQ && board.pieces[D1] == EMPTY_PIECE &&
             board.pieces[C1] == EMPTY_PIECE &&
             board.pieces[B1] == EMPTY_PIECE && !is_attacked(E1, board.player) &&
             !is_attacked(D1, board.player) && !is_attacked(C1, board.player)) {
-            add_move(moves, count++, E1, C1, CASTLE_WQ);
+            moves[count++] = create_move(E1, C1, CASTLE_WQ);
         }
     } else {
         if (board.castle & CASTLE_BK && board.pieces[F8] == EMPTY_PIECE &&
             board.pieces[G8] == EMPTY_PIECE && !is_attacked(E8, board.player) &&
             !is_attacked(F8, board.player) && !is_attacked(G8, board.player)) {
-            add_move(moves, count++, E8, G8, CASTLE_BK);
+            moves[count++] = create_move(E8, G8, CASTLE_BK);
         }
         if (board.castle & CASTLE_BQ && board.pieces[D8] == EMPTY_PIECE &&
             board.pieces[C8] == EMPTY_PIECE &&
             board.pieces[B8] == EMPTY_PIECE && !is_attacked(E8, board.player) &&
             !is_attacked(D8, board.player) && !is_attacked(C8, board.player)) {
-            add_move(moves, count++, E8, C8, CASTLE_BQ);
+            moves[count++] = create_move(E8, C8, CASTLE_BQ);
         }
     }
 
@@ -150,47 +133,47 @@ static int generate_pawn_move(Move *moves, int count, Fast start) {
         /* Adds all promotion moves */
         if (get_rank(start) == 7 - second) {
             for (int i = PROMOTION_N; i <= PROMOTION_Q; i++) {
-                add_move(moves, count++, start, end, i);
+                moves[count++] = create_move(start, end, i);
             }
 
             /* Adds promotion capture moves */
             if (!invalid_square(end + RIGHT) &&
                 board.colors[end + RIGHT] == 3 - board.player) {
                 for (int i = PROMOTION_N; i <= PROMOTION_Q; i++) {
-                    add_move(moves, count++, start, end + RIGHT, i);
+                    moves[count++] = create_move(start, end + RIGHT, i);
                 }
             }
             if (!invalid_square(end + LEFT) &&
                 board.colors[end + RIGHT] == 3 - board.player) {
                 for (int i = PROMOTION_N; i <= PROMOTION_Q; i++) {
-                    add_move(moves, count++, start, end + LEFT, i);
+                    moves[count++] = create_move(start, end + LEFT, i);
                 }
             }
 
         } else {
             /* Adds move and double move if on special rank */
-            add_move(moves, count++, start, end, NORMAL);
+            moves[count++] = create_move(start, end, NORMAL);
             if (get_rank(start) == second && !board.colors[end + direction]) {
-                add_move(moves, count++, start, end + direction, DOUBLE);
+                moves[count++] = create_move(start, end + direction, DOUBLE);
             }
         }
     }
 
     /* Adds pawn attacks to the right and left and en passant */
-    int i = end + RIGHT;
-    if (!invalid_square(i) && board.colors[i] != board.player) {
-        if (board.colors[i] != EMPTY_COLOR) {
-            add_move(moves, count++, start, i, CAPTURE);
-        } else if (i == board.enpassant) {
-            add_move(moves, count++, start, i, ENPASSANT);
+    Fast attack = end + RIGHT;
+    if (!invalid_square(attack) && board.colors[attack] != board.player) {
+        if (board.colors[attack] != EMPTY_COLOR) {
+            moves[count++] = create_move(start, attack, CAPTURE);
+        } else if (attack == board.enpassant) {
+            moves[count++] = create_move(start, attack, ENPASSANT);
         }
     }
-    i = end + LEFT;
-    if (!invalid_square(i) && board.colors[i] != board.player) {
-        if (board.colors[i] != EMPTY_COLOR) {
-            add_move(moves, count++, start, i, CAPTURE);
-        } else if (i == board.enpassant) {
-            add_move(moves, count++, start, i, ENPASSANT);
+    attack = end + LEFT;
+    if (!invalid_square(attack) && board.colors[attack] != board.player) {
+        if (board.colors[attack] != EMPTY_COLOR) {
+            moves[count++] = create_move(start, attack, CAPTURE);
+        } else if (attack == board.enpassant) {
+            moves[count++] = create_move(start, attack, ENPASSANT);
         }
     }
 
@@ -206,9 +189,9 @@ static int generate_piece_move(Move *moves, int count, Fast start, Fast piece) {
             Fast end = start + vectors[piece - 2][i];
             if (!invalid_square(end)) {
                 if (board.colors[end] == EMPTY_COLOR) {
-                    add_move(moves, count++, start, end, NORMAL);
+                    moves[count++] = create_move(start, end, NORMAL);
                 } else if (board.colors[end] == 3 - board.player) {
-                    add_move(moves, count++, start, end, CAPTURE);
+                    moves[count++] = create_move(start, end, CAPTURE);
                 }
             }
         }
@@ -223,9 +206,9 @@ static int generate_piece_move(Move *moves, int count, Fast start, Fast piece) {
                  end += vectors[piece - 2][i]) {
                 /* Adds normal move or capture move */
                 if (board.colors[end] == EMPTY_COLOR) {
-                    add_move(moves, count++, start, end, NORMAL);
+                    moves[count++] = create_move(start, end, NORMAL);
                 } else if (board.colors[end] == 3 - board.player) {
-                    add_move(moves, count++, start, end, CAPTURE);
+                    moves[count++] = create_move(start, end, CAPTURE);
                 }
                 /* Stops traversal once a piece is hit*/
                 if (board.colors[end]) {
