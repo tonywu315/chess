c1cfa09573d32257c38a9224c1691008f288872d
Tony Wu <tonywu315@gmail.com>
2021-11-07 23:12:40 -0500

    Create Continuous Integration Tests through CMake and Github Actions

--- include/benchmark.h
+++ include/benchmark.h
@@ -3,6 +3,7 @@
 
 #include "constants.h"
 
+void perft(int depth, U64 *nodes);
 void benchmark(int depth);
 
 #endif
--- src/benchmark.c
+++ src/benchmark.c
@@ -3,8 +3,6 @@
 #include "move.h"
 #include "move_generation.h"
 
-typedef unsigned long long U64;
-
 /*
 CURRENT PERFORMANCE FOR DEPTH 6
 perft:          2.658 MNPS
@@ -14,7 +12,7 @@ loop_speed:     609.352 MNPS
 */
 
 /* Performance test for enumerating all moves to a certain depth */
-static inline void perft(int depth, U64 *nodes) {
+void perft(int depth, U64 *nodes) {
     Move move_list[MAX_MOVES];
 
     if (depth == 0) {
@@ -33,7 +31,7 @@ static inline void perft(int depth, U64 *nodes) {
 }
 
 /* Performance test for enumerating mostly legal moves to a certain depth */
-static inline void speedy_perft(int depth, U64 *nodes) {
+static void speedy_perft(int depth, U64 *nodes) {
     Move move_list[MAX_MOVES];
     int count = generate_moves(move_list);
 
@@ -51,7 +49,7 @@ static inline void speedy_perft(int depth, U64 *nodes) {
 }
 
 /* Performance test for enumerating all pseudo legal moves to a certain depth */
-static inline void pseudo_perft(int depth, U64 *nodes) {
+static void pseudo_perft(int depth, U64 *nodes) {
     Move move_list[MAX_MOVES];
     int count = generate_moves(move_list);
 
