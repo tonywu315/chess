e18287316480d01c40065240ed82b45fc2df061a
Tony Wu <tonywu315@gmail.com>
2022-06-04 22:37:43 -0400

    Simplify MVV/LVA ordering

--- include/move_order.h
+++ include/move_order.h
@@ -11,17 +11,53 @@ typedef struct moveList {
 /*
     Move Ordering
 
-    1. Hash Move
-    2. Winning captures
+    1. Transposition table move
+    2. Winning captures with MVV/LVA
     3. Equal captures
     4. Killer moves
-    5. Losing captures
-    6. Quiet moves
+    5. Quiet moves
+    6. Losing captures
 */
 enum MoveValue {
     TT_MOVE = 100,
-    KILLER_MOVE = -1,
-    QUIET_MOVE = -100,
+    WINNING_CAPTURE = 80,
+    EQUAL_CAPTURE = 60,
+    KILLER_MOVE = 40,
+    QUIET_MOVE = 20,
+    LOSING_CAPTURE = 0,
+};
+
+enum MVV_LVA {
+    QxP = LOSING_CAPTURE,
+    RxP,
+    BxP,
+    NxP,
+    QxN,
+    QxB,
+    RxN,
+    RxB,
+    QxR,
+    PxP = EQUAL_CAPTURE,
+    BxN,
+    NxN,
+    BxB,
+    NxB,
+    RxR,
+    QxQ,
+    KxP = WINNING_CAPTURE,
+    KxN,
+    PxN,
+    KxB,
+    PxB,
+    KxR,
+    BxR,
+    NxR,
+    PxR,
+    KxQ,
+    RxQ,
+    BxQ,
+    NxQ,
+    PxQ,
 };
 
 void score_moves(Board *board, Move *moves, MoveList *move_list, int length,
--- src/move_order.c
+++ src/move_order.c
@@ -1,90 +1,39 @@
 #include "move_order.h"
 
+static inline int mvv_lva(int attacker, int victim);
+
 // Score each move
 void score_moves(Board *board, Move *moves, MoveList *move_list, int length,
                  Move tt_move) {
-    // clang-format off
-    const int mvv_lva[][15] = {
-        [W_QUEEN] = {
-            [W_PAWN] = 15,   [B_PAWN] = 15,   [W_KNIGHT] = 14, [B_KNIGHT] = 14,
-            [W_BISHOP] = 13, [B_BISHOP] = 13, [W_ROOK] = 12,   [B_ROOK] = 12,
-            [W_QUEEN] = 1,   [B_QUEEN] = 1,   [W_KING] = 11,   [B_KING] = 11,
-        },
-        [B_QUEEN] = {
-            [W_PAWN] = 15,   [B_PAWN] = 15,   [W_KNIGHT] = 14, [B_KNIGHT] = 14,
-            [W_BISHOP] = 13, [B_BISHOP] = 13, [W_ROOK] = 12,   [B_ROOK] = 12,
-            [W_QUEEN] = 1,   [B_QUEEN] = 1,   [W_KING] = 11,   [B_KING] = 11,
-        },
-        [W_ROOK] = {
-            [W_PAWN] = 9,    [B_PAWN] = 9,    [W_KNIGHT] = 8, [B_KNIGHT] = 8,
-            [W_BISHOP] = 7,  [B_BISHOP] = 7,  [W_ROOK] = 1,   [B_ROOK] = 1,
-            [W_QUEEN] = -13, [B_QUEEN] = -13, [W_KING] = 6,   [B_KING] = 6,
-        },
-        [B_ROOK] = {
-            [W_PAWN] = 9,    [B_PAWN] = 9,    [W_KNIGHT] = 8, [B_KNIGHT] = 8,
-            [W_BISHOP] = 7,  [B_BISHOP] = 7,  [W_ROOK] = 1,   [B_ROOK] = 1,
-            [W_QUEEN] = -13, [B_QUEEN] = -13, [W_KING] = 6,   [B_KING] = 6,
-        },
-        [W_BISHOP] = {
-            [W_PAWN] = 5,    [B_PAWN] = 5,    [W_KNIGHT] = 2, [B_KNIGHT] = 2,
-            [W_BISHOP] = 1,  [B_BISHOP] = 1,  [W_ROOK] = -12, [B_ROOK] = -12,
-            [W_QUEEN] = -16, [B_QUEEN] = -16, [W_KING] = 4,   [B_KING] = 4,
-        },
-        [B_BISHOP] = {
-            [W_PAWN] = 5,    [B_PAWN] = 5,    [W_KNIGHT] = 2, [B_KNIGHT] = 2,
-            [W_BISHOP] = 1,  [B_BISHOP] = 1,  [W_ROOK] = -12, [B_ROOK] = -12,
-            [W_QUEEN] = -16, [B_QUEEN] = -16, [W_KING] = 4,   [B_KING] = 4,
-        },
-        [W_KNIGHT] = {
-            [W_PAWN] = 5,    [B_PAWN] = 5,    [W_KNIGHT] = 1, [B_KNIGHT] = 1,
-            [W_BISHOP] = 0,  [B_BISHOP] = 0,  [W_ROOK] = -12, [B_ROOK] = -12,
-            [W_QUEEN] = -16, [B_QUEEN] = -16, [W_KING] = 4,   [B_KING] = 4,
-        },
-        [B_KNIGHT] = {
-            [W_PAWN] = 5,    [B_PAWN] = 5,    [W_KNIGHT] = 1, [B_KNIGHT] = 1,
-            [W_BISHOP] = 0,  [B_BISHOP] = 0,  [W_ROOK] = -12, [B_ROOK] = -12,
-            [W_QUEEN] = -16, [B_QUEEN] = -16, [W_KING] = 4,   [B_KING] = 4,
-        },
-        [W_PAWN] = {
-            [W_PAWN] = 0,     [B_PAWN] = 0,     [W_KNIGHT] = -11, [B_KNIGHT] = -11,
-            [W_BISHOP] = -11, [B_BISHOP] = -11, [W_ROOK] = -14,   [B_ROOK] = -14,
-            [W_QUEEN] = -18,  [B_QUEEN] = -18,  [W_KING] = 3,     [B_KING] = 3,
-        },
-        [B_PAWN] = {
-            [W_PAWN] = 0,     [B_PAWN] = 0,     [W_KNIGHT] = -11, [B_KNIGHT] = -11,
-            [W_BISHOP] = -11, [B_BISHOP] = -11, [W_ROOK] = -14,   [B_ROOK] = -14,
-            [W_QUEEN] = -18,  [B_QUEEN] = -18,  [W_KING] = 3,     [B_KING] = 3,
-        },
-    };
-    // clang-format on
-
     for (int i = 0; i < length; i++) {
         Move move = moves[i];
+
         if (move == tt_move) {
             move_list[i].move = move;
             move_list[i].score = TT_MOVE;
         } else {
-            int start = get_move_start(move);
-            int end = get_move_end(move);
-            int capture = board->board[end];
             int flag = get_move_flag(move);
 
             if (flag == PROMOTION) {
                 move_list[i].move = move;
-                move_list[i].score = 50;
+                move_list[i].score = PxR + get_move_promotion(move);
             } else if (flag == ENPASSANT) {
                 move_list[i].move = move;
-                move_list[i].score = 0;
+                move_list[i].score = PxP;
             } else if (flag == CASTLING) {
                 move_list[i].move = move;
-                move_list[i].score = -50;
-            } else if (capture != NO_PIECE) {
-                move_list[i].move = moves[i];
-                move_list[i].score = mvv_lva[capture][board->board[start]];
+                move_list[i].score = QUIET_MOVE + 1;
             } else {
-                move_list[i].move = moves[i];
-                move_list[i].score = QUIET_MOVE;
-            }  
+                int capture = board->board[get_move_end(move)];
+                if (capture != NO_PIECE) {
+                    move_list[i].move = moves[i];
+                    move_list[i].score =
+                        mvv_lva(board->board[get_move_start(move)], capture);
+                } else {
+                    move_list[i].move = moves[i];
+                    move_list[i].score = QUIET_MOVE;
+                }
+            }
         }
     }
 }
@@ -104,4 +53,25 @@ Move sort_moves(MoveList *move_list, int length, int index) {
     move_list[index] = best_move;
 
     return best_move.move;
+}
+
+// Most Valuable Victim - Least Valuable Attacker heuristic to sort moves
+static inline int mvv_lva(int attacker, int victim) {
+    static const int mvv_lva[][13] = {
+        {PxP, PxN, PxB, PxR, PxQ, 0, 0, 0, PxP, PxN, PxB, PxR, PxQ},
+        {NxP, NxN, NxB, NxR, NxQ, 0, 0, 0, NxP, NxN, NxB, NxR, NxQ},
+        {BxP, BxN, BxB, BxR, BxQ, 0, 0, 0, BxP, BxN, BxB, BxR, BxQ},
+        {RxP, RxN, RxB, RxR, RxQ, 0, 0, 0, RxP, RxN, RxB, RxR, RxQ},
+        {QxP, QxN, QxB, QxR, QxQ, 0, 0, 0, QxP, QxN, QxB, QxR, QxQ},
+        {KxP, KxN, KxB, KxR, KxQ, 0, 0, 0, KxP, KxN, KxB, KxR, KxQ},
+        {},
+        {},
+        {PxP, PxN, PxB, PxR, PxQ, 0, 0, 0, PxP, PxN, PxB, PxR, PxQ},
+        {NxP, NxN, NxB, NxR, NxQ, 0, 0, 0, NxP, NxN, NxB, NxR, NxQ},
+        {BxP, BxN, BxB, BxR, BxQ, 0, 0, 0, BxP, BxN, BxB, BxR, BxQ},
+        {RxP, RxN, RxB, RxR, RxQ, 0, 0, 0, RxP, RxN, RxB, RxR, RxQ},
+        {QxP, QxN, QxB, QxR, QxQ, 0, 0, 0, QxP, QxN, QxB, QxR, QxQ},
+        {KxP, KxN, KxB, KxR, KxQ, 0, 0, 0, KxP, KxN, KxB, KxR, KxQ},
+    };
+    return mvv_lva[attacker][victim];
 }
