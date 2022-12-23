fdca63c5eece83057d7796f2a72bf509e90155b4
Tony Wu <tonywu315@gmail.com>
2022-12-22 23:50:48 -0500

    Improve robustness of FEN parsing

--- include/board.h
+++ include/board.h
@@ -1,12 +1,12 @@
-#ifndef BITBOARD_H
-#define BITBOARD_H
+#ifndef BOARD_H
+#define BOARD_H
 
 #include "types.h"
 
 void init_board(Board *board);
 void print_board(const Board *board, int score, bool game_over);
 void print_bitboard(Bitboard bitboard);
-void load_fen(Board *board, const char *fen);
+bool load_fen(Board *board, const char *fen);
 char *get_coordinates(int square);
 
 #endif
