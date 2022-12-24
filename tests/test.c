dfa1b7e40f7077877341afa763c383149f3ffbdb
Tony Wu <tonywu315@gmail.com>
2022-12-23 21:40:21 -0500

    Fix perft test

--- tests/test.c
+++ tests/test.c
@@ -6,12 +6,12 @@
 #define TEST_DEPTH 5
 
 static const char *positions[TEST_POSITIONS] = {
-    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -",
-    "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ",
-    "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -",
-    "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq -",
-    "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ -",
-    "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - -"};
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+    "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 15",
+    "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 1 60",
+    "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 2 30",
+    "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 0 10",
+    "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 4 30"};
 
 static const U64 perft_results[TEST_POSITIONS][TEST_DEPTH] = {
     {20, 400, 8902, 197281, 4865609},     {48, 2039, 97862, 4085603, 193690690},
